1038624081aca0e8c5d9a5d031e2ecf43df4d6d7
Ryan Cheung <cheungcm2010@gmail.com>
2021-08-26 15:53:30 +0800

    Added task status remind on top of MainActivity

--- app/src/main/java/com/ccm/searchmusic/MainActivity.kt
+++ app/src/main/java/com/ccm/searchmusic/MainActivity.kt
@@ -5,7 +5,6 @@
 package com.ccm.searchmusic
 
 import android.os.Bundle
-import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -15,14 +14,30 @@ import com.ccm.searchmusic.ui.music.Music
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
- * Due to insufficient time, all will keep simple as possible:
- * - Language: Kotlin
- * - Deployment Target: Android 7.0+
- * - Compose
- * - MVVM
- * - Retrofit
- * - Dagger Hilt
- * - UI: just use a play arrow, not using player bar binding with notification
+ * All will keep simple as possible to meet assignment purpose and using:
+ * - MVVM pattern (pure)
+ * - Creating layout programmatically (JetPack Compose)
+ *
+ *
+ * Android Developer Assignment:
+    The assignment is simple and straightforward. However, the developer is expected to finish it with his/her highest standard.
+    It is intended to test the developer's software architect skills, design patterns, and coding styles.
+
+    ● Task: Create an app that let user to search music in iTunes Music.
+            The search results shall be displayed in RecyclerView and user can play the preview track.
+
+    ● API: iTunes Search API (DONE)
+    ● Language: Kotlin (DONE)
+    ● Deployment Target: Android 7.0+ (DONE)
+    ● The following are the suggested tools, or anything that can show your skills:
+        ○ Design pattern: MVVM, MVP (DONE - MVVM)
+        ○ Api Client: Retrofit (DONE)
+        ○ Dependency injection: Dragger, Koin (DONE - Dagger Hilt)
+    ● The following are nice to include to your project:
+        ○ Practice of coding style (DONE)
+        ○ Structure between Activities and Fragments (DONE - One Activity)
+        ○ Organisation of dimension and style (DONE - compose files)
+        ○ Documentation / Descriptions
  * */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
