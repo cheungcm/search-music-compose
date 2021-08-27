fcfbd3a427b064264a28d8e16e3dfc609fc8c599
Ryan Cheung <cheungcm2010@gmail.com>
2021-08-27 10:39:03 +0800

    Finetune part of dependencies

--- buildSrc/src/main/java/com/searchmusic/buildSrc/Deps.kt
+++ buildSrc/src/main/java/com/searchmusic/buildSrc/Deps.kt
@@ -5,25 +5,71 @@ object Deps {
     object Android {
 
         object Compose {
-            const val version = "1.0.0"
-            const val navigationVersion = "2.4.0-alpha03"
-            const val activityVersion = "1.3.1"
+            private const val version = "1.0.0"
+            private const val navigationVersion = "2.4.0-alpha03"
 
-            const val runtime = "androidx.compose.runtime:runtime:1.0.0-beta09"
-            const val navigation = "androidx.navigation:navigation-compose:$navigationVersion"
-            const val liveData = "androidx.compose.runtime:runtime-livedata:$version"
             const val ui = "androidx.compose.ui:ui:$version"
-            const val uiUtil = "androidx.compose.ui:ui-util:$version"
-            const val uiTooling = "androidx.compose.ui:ui-tooling:$version"
             const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
             const val materialDesign = "androidx.compose.material:material:$version"
-            const val materialDesignIcons = "androidx.compose.material:material-icons-core:$version"
-            const val materialDesignIconsExtended = "androidx.compose.material:material-icons-extended:$version"
-            const val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02"
-            const val activity = "androidx.activity:activity-compose:$activityVersion"
+            const val runtime = "androidx.compose.runtime:runtime:1.0.0-beta09"
+            const val navigation = "androidx.navigation:navigation-compose:$navigationVersion"
+            const val liveData = "androidx.compose.runtime:runtime-livedata:$version"
             const val viewModels = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
-            const val paging = "androidx.paging:paging-compose:1.0.0-alpha12"
         }
+    }
+
+    object Accompanist {
+        private const val version = "0.16.1"
+
+        const val insets = "com.google.accompanist:accompanist-insets:$version"
+    }
+
+    object Coil {
+        private const val version = "1.3.2"
+
+        const val compose = "io.coil-kt:coil-compose:$version"
+    }
+
+    object Dagger {
+        private const val version = "2.38.1"
+
+        const val hilt = "com.google.dagger:hilt-android:$version"
+        const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
+        const val hiltCompose = "androidx.hilt:hilt-navigation-compose:1.0.0-alpha03"
+    }
+
+    object Retrofit {
+        private const val version = "2.9.0"
+
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
+    }
+
+    object OkHttp {
+        private const val version = "5.0.0-alpha.2"
+
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val logger = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object Moshi {
+        private const val version = "1.12.0"
+
+        const val moshi = "com.squareup.moshi:moshi-kotlin:$version"
+        const val kotlinCodegen = "com.squareup.moshi:moshi-kotlin-codegen:$version"
+    }
+
+    object ExoPlayer {
+        private const val version = "2.15.0"
+
+        const val exoPlayer = "com.google.android.exoplayer:exoplayer-core:$version"
+        const val exoPlayerOkhttp = "com.google.android.exoplayer:extension-okhttp:$version"
+    }
+
+    object Timber {
+        private const val version = "5.0.1"
+
+        const val timber = "com.jakewharton.timber:timber:$version"
     }
 }
